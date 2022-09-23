baa99c4d95d1354fb1a35207f493883025b48a24
chjsdmn <chjsdmn@hotmail.com>
2022-09-23 16:30:12 +0200

    added Symptom.class under Moduls

---
+++ Oblig-1-ITPE3200/Models/Symptom.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oblig_1_ITPE3200.Models
+{
+    public class Symptom
+    {
+    }
+}
