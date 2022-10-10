36e4d9bdbc0919cd97a84282e3bee7d63add1569
zkreso <zkreso@gmail.com>
2022-10-10 12:48:42 +0200

    Changed name of class

---
+++ Oblig-1-ITPE3200/DTOs/DiseaseListDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Oblig_1_ITPE3200.DTOs
+{
+    public class DiseaseListDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string[] Symptoms { get; set; }
+    }
+}
