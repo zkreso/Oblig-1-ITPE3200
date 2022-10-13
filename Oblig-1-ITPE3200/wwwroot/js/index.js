057935d849891239d8e28d2fcd0b3e4446945394
Sigurd Skyrud <69970004+sigzugern@users.noreply.github.com>
2022-10-14 00:21:00 +0200

    Made js scripting for getting all symptoms form server and moving clicket option in select to div underneath.

--- Oblig-1-ITPE3200/wwwroot/js/index.js
+++ Oblig-1-ITPE3200/wwwroot/js/index.js
@@ -1,1 +1,22 @@
-﻿
+﻿$(function () {
+    getAllSymptoms();
+});
+
+function getAllSymptoms() {
+    $.get("oblig/GetAllSymptoms", function (sList) {
+        let ut = "";
+
+        for (let i = 0; i < sList.length; i++) {
+
+            ut += '<option id="' + sList[i].id + '">' + sList[i].name +'</option>';
+        }
+        $("#dList").append(ut);
+    });
+}
+
+function addSymptom(s) {
+    let id = $(s).attr("id");
+    let name = $(s).val();
+
+    $("#outSymptom").append(name+"<br>");
+}
