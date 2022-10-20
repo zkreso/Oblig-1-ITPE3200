7a64f8ed9afc4180be5f632e9387336edaae806a
zkreso <zkreso@gmail.com>
2022-10-20 23:35:33 +0200

    first iteration

--- Oblig-1-ITPE3200/wwwroot/js/search.js
+++ Oblig-1-ITPE3200/wwwroot/js/search.js
@@ -1,44 +1,82 @@
-﻿$(function () {
+﻿var selectedSymptoms = [];
+
+$(function () {
     getallsymptoms();
 })
 
 function getallsymptoms() {
     $.get("oblig/GetAllSymptoms", function (symptoms) {
-        skrivUtSymptoms(symptoms);
+        printAllSymptoms(symptoms);
     });
 }
 
-function skrivUtSymptoms(symptoms) {
-    let ut = "";
+function printAllSymptoms(symptoms) {
+    let ut = "<table class='table table-hover'><thead><tr>" +
+        "<thead><tr>" +
+        "<th scope='col'>Id</th><th scope='col'>Name</th><th scope='col'>Select</th>" +
+        "</tr></thead><tbody>";
     for (let symptom of symptoms) {
-        ut += "<div class='form-check'>";
-        ut += "<input class='form-check-input' name='symptoms' type='checkbox' value='" + symptom.id + "' id='" + symptom.name + "'>";
-        ut += "<label class ='form-check-label' for='" + symptom.name + "'>" + symptom.name + "</label>";
-        ut += "</div>";
+        ut += "<tr>" +
+            "<th scope='row'>" + symptom.id + "</td>" +
+            "<td>" + symptom.name + "</td>" +
+            "<td><a href='#' onclick='select(" + symptom.id + ", \"" + symptom.name + "\")'>Select</td></tr>";
     }
     $("#symptoms").html(ut);
 }
 
+function select(sid, sname) {
+    let newSymptom = {
+        "name": sname,
+        "id" : sid
+    }
+    selectedSymptoms.push(newSymptom);
+
+    printSelectedSymptoms();
+    search();
+}
+
+function deselect(sid) {
+    let i = selectedSymptoms.findIndex(s => s.id == sid);
+    selectedSymptoms.splice(i, 1);
+
+    printSelectedSymptoms();
+    search();
+}
+
+function printSelectedSymptoms() {
+    let ut = "";
+    if (selectedSymptoms.length == 0) {
+        ut += "<p class='text-muted'>No symptoms selected</p>";
+    }
+
+    for (let symptom of selectedSymptoms) {
+        ut += "<button type='button' class='btn btn-primary m-1' onclick='deselect(" + symptom.id + ")'>" +
+            "<i class='bi bi-x-circle text-light'></i> " + symptom.name + "</button>";
+    }
+    $("#selectedsymptomsdiv").html(ut);
+}
+
 function search() {
-    
-    // Creating array of symptom id's to send to server
-    let formData = document.getElementsByName("symptoms");
-    let symptomsArray = [];
-    for (let entry of formData) {
-        if (entry.checked) {
-            symptomsArray.push(entry.value);
-        }
+    if (selectedSymptoms.length == 0) {
+        let ut = "<div class='text-muted'>No symptoms selected</div>";
+        $("#results").html(ut);
+        return;
     }
 
     // Creating table of matches to show to user
-    let resultsUt = "<h3>Matching diseases:</h3>" +
-        "<table class='table table-striped'>" +
-        "<tr><th>Disease</th><th>Symptoms</th></tr>";
+    let resultsUt = "<table class='table'>" +
+        "<tr><th>Name</th><th>Symptoms</th></tr>";
+
+    let symptomsArray = [];
+    for (let symptom of selectedSymptoms) {
+        symptomsArray.push(symptom.id);
+    }
 
     $.post("oblig/SearchDiseases", $.param({ symptomsArray }, true), function (diseases) {
         // Check for empty results
-        if (diseases == null) {
-            $("#results").html(resultsUt);
+        if (diseases.length == 0) {
+            let ut = "<div class='text-danger'>No matching diseases</div>";
+            $("#results").html(ut);
             return;
         }
 
