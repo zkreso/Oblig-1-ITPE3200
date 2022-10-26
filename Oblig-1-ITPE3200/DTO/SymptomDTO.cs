91119d84a09c4dc2cd8c65c02771dd85f9fd856f
Sigurd Skyrud <69970004+sigzugern@users.noreply.github.com>
2022-10-26 17:04:18 +0200

    Added DTOs. Not used yet, maybe future

---
+++ Oblig-1-ITPE3200/DTO/SymptomDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Oblig_1_ITPE3200.Models;
+
+namespace Oblig_1_ITPE3200.DTO
+{
+	public class SymptomDTO
+	{
+		public SymptomDTO()
+		{
+			Symptom symptom = new Symptom();
+		}
+	}
+}
+
