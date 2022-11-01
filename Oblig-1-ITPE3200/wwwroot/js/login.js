ac3e928388c9b5ca86d142431077803c2620ead1
sigzugern <skyruden@gmail.com>
2022-11-01 14:19:51 +0100

    Added login and login validation on client side. Functions check for right typing and send login post to server.

---
+++ Oblig-1-ITPE3200/wwwroot/js/login.js
@@ -0,0 +1,23 @@
+﻿function logIn() {
+    const usernameOk = validateUsername($("#username").val());
+    const passwordOk = validatePassword($("#password").val());
+
+    if (usernameOk && passwordOk) {
+        const user = {
+            username: $("#username").val(),
+            password: $("#password").val()
+        }
+
+        $.post("oblig/LogIn", user, function (OK) {
+            if (OK) {
+                window.location.href = "admin.html";
+            }
+            else {
+                $("#err").html("Wrong in username and password.");
+            }
+        }).fail(function () {
+            $("#err").html("Somthing wrong accured on server. Try again later.");
+        });
+    }
+}
+
---
+++ Oblig-1-ITPE3200/wwwroot/js/validate.js
@@ -0,0 +1,28 @@
+﻿function validateUsername(username) {
+    const regExp = /^[a-zA-ZæøåÆØÅ\.\ ]{2,20}$/;
+    const ok = regExp.test(username);
+
+    if (ok) {
+        $("#errUser").html("");
+        return true;
+    }
+    else {
+        $("#errUser").html("Username must be 2 to 20 characters long.");
+        return false;
+    }
+}
+
+function validatePassword(password) {
+    const regExp = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    const ok = regExp.test(password);
+
+    if (ok) {
+        $("#errPass").html("");
+        return true;
+    }
+    else {
+        $("#errPass").html("Password must be at least 8 characters long, and have one capital letter and one number");
+        return false;
+    }
+}
+
